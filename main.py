235d9bd352266da5b1fe45d9ac181ad1d473a100
profffff <n.vybornov@icloud.com>
2022-11-05 12:00:31 +0300

    Update

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from time import sleep
 from random import shuffle
 from io import TextIOWrapper
+import os
+import logging
+from logging.handlers import RotatingFileHandler
 #from flaskext.noextref import NoExtRef
 
 main = Flask(__name__)
@@ -1001,3 +1004,33 @@ def kursovaya_win():
 
 if __name__ == "__main__":
     main.run(debug=True)
+
+class Config(object):
+    # ...
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+
+if __name__ == "__init__":
+    def create_app(config_class=Config):
+        # ...
+        if not main.debug and not main.testing:
+            # ...
+
+            if main.config['LOG_TO_STDOUT']:
+                stream_handler = logging.StreamHandler()
+                stream_handler.setLevel(logging.INFO)
+                main.logger.addHandler(stream_handler)
+            else:
+                if not os.path.exists('logs'):
+                    os.mkdir('logs')
+                file_handler = RotatingFileHandler('logs/main.log',
+                                                   maxBytes=10240, backupCount=10)
+                file_handler.setFormatter(logging.Formatter(
+                    '%(asctime)s %(levelname)s: %(message)s '
+                    '[in %(pathname)s:%(lineno)d]'))
+                file_handler.setLevel(logging.INFO)
+                main.logger.addHandler(file_handler)
+
+            main.logger.setLevel(logging.INFO)
+            main.logger.info('Main startup')
+
+        return main
