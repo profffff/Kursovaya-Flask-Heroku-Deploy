9483d49a7c6185e27821342f9352130c3f247714
profffff <n.vybornov@icloud.com>
2022-11-14 00:58:12 +0300

    config for heroku db

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import os
 
 #DB_URL = 'postgres://cssuehtndgmavj:4a96332271add397fcf4ede36bbb3fa94a77ab2329f78c4f051d2c5ac306fd58@ec2-54-91-223-99.compute-1.amazonaws.com:5432/d18idpvuarqsho'
-# DB_NAME = 'd18idpvuarqsho'
-# DB_USER = 'cssuehtndgmavj'
-# DB_PASS = '4a96332271add397fcf4ede36bbb3fa94a77ab2329f78c4f051d2c5ac306fd58'
-# DB_PORT = '5432'
-# DB_HOST = 'ec2-54-91-223-99.compute-1.amazonaws.com'
-
-DB_NAME = 'words'
-DB_USER = 'admin'
-DB_PASS = 'admin'
-DB_HOST = 'localhost'
+DB_NAME = 'd18idpvuarqsho'
+DB_USER = 'cssuehtndgmavj'
+DB_PASS = '4a96332271add397fcf4ede36bbb3fa94a77ab2329f78c4f051d2c5ac306fd58'
 DB_PORT = '5432'
+DB_HOST = 'ec2-54-91-223-99.compute-1.amazonaws.com'
+
+# DB_NAME = 'words'
+# DB_USER = 'admin'
+# DB_PASS = 'admin'
+# DB_HOST = 'localhost'
+# DB_PORT = '5432'
 
 class Config(object):
     # ...
